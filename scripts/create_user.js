fee5947ccf81a8e34c810fe937d45a81a96c2c37
Joan Miquel Espada <joanmi@espada.cat>
2023-01-19 13:50:53 +0100

    user creation script

--- scripts/create_user.js
+++ scripts/create_user.js
@@ -1,7 +1,6 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import crypto from "crypto";
 import { Command } from 'commander';
 const program = new Command();
 import { dynamodb } from './ddbClient.js';
@@ -9,32 +8,26 @@ import { Config } from './configuration.js';
 import { PutItemCommand } from "@aws-sdk/client-dynamodb";
 const config = Config();
 
-import { schemas } from './tableDefinitions.js';
+async function create_user(options) {
 
-async function create_adminuser() {
+    let user_id = options.id; 
+    let user_email = options.email; 
+    let user_device = options.device; 
+    let user_walletAddress = options.wallet; 
 
-    let user_admin_id = 'e8c9284b-a8e9-466d-87de-d2d3d8e1ab8f'
-    let user_admin_email = "dfcgw345f03ger@truly.camera"
-    let user_admin_device = "device-12DFFG34-12YUIO34-12QWE34-8790"
-    let user_admin_walletAddress = "00x00";
-
-    //let cyphered = crypto.createHmac('sha256', secret)
-    //var encrypt = cyphered.update(user_admin_passw); //, 'utf8', 'hex'); //.digest("hex");
-    //let gen_hmac = '';
-    //console.log("password admin: "+ user_admin_passw +" cyphered: " + gen_hmac );
 
     await Promise.all([new Promise(async (resolve, error) => {
         var params = {
             TableName: config.dbSchemaNames.users ,
             Item: {
-                'userID': { S: user_admin_id },
+                'userID': { S: user_id },
                 'creationTime': { S: new Date().toISOString() },
-                'walletAddress': { S: user_admin_walletAddress },
-                'email': { S: user_admin_email },
+                'walletAddress': { S: user_walletAddress },
+                'email': { S: user_email },
                 'password': {S: 'NULL' },
-                'device': { S: user_admin_device  },
+                'device': { S: user_device  },
                 'userRoles': { SS: ['Basic', 'Admin'] },
-                'userStatus': { S: 'Enabled' },
+                'userStatus': { S: 'Disabled' },
             }
         };
         try {
@@ -49,7 +42,19 @@ async function create_adminuser() {
     })]);
 };
 
+program
+    .name('create admin user')
+    .description('create admin user')
+    .version('0.0.1')
+    .option('-e, --email <email>','user email')
+    //.option('-p, --password <password>','user pass')
+    .option('-d, --device <device>', 'user device')
+    .option('-w, --wallet <wallet address>', 'user wallet address')
+    .option('-i, --id <user identifier>', 'user identifier')
 
 
+program.parse(process.argv);
 
-create_adminuser();
+const options= program.opts();
+
+create_user(options);
