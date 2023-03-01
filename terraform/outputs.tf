acbfd737d80b8151cbe6bd03666f515be67ee0fc
Joan Miquel Espada <joanmi@espada.cat>
2023-03-01 10:26:04 +0100

    retrieve interface topics

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,10 @@
 output "invoke_url" {
   value = aws_apigatewayv2_stage.truly_stage.invoke_url
 }
+output "video_in_topic" {
+  value = aws_sns_topic.video_in_topic.arn
+}
+
+output "video_out_topic" {
+  value = aws_sns_topic.video_out_topic.arn
+}
